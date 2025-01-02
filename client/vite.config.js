6eb62b14b492f3434b6ec0e040f37394da3a8193
sandhyasawalkar1999 <sandhyasawalkar1999@gmail.com>
2025-01-02 16:30:42 +0530

    complete signup functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // If your backend is using HTTP
+        changeOrigin: true,
+        secure: false, // Set false if backend doesn't use HTTPS
+      },
+    },
+  },
+
   plugins: [react()],
 })
